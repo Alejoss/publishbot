d78a980c21e303bd5840e13c6a2d25cad0e9687d
Alejandro Veintimilla <alejoveintimilla@gmail.com>
2023-03-17 16:06:58 -0500

    Postgres, Docker.

--- publishbot/settings.py
+++ publishbot/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'publishbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'publishbot_db',
+        'USER': 'alejoss',
+        'PASSWORD': 'farseer99',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
